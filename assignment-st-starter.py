6974158ee2f28878b797c1ce163d590110d8de49
xyyyc123 <1443268879@qq.com>
2024-09-26 22:55:38 +0800

    Add files via upload

---
+++ assignment-st-starter.py
@@ -0,0 +1,40 @@
+# import packages
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+# show the title
+st.title('Titanic App By Yachen Xu')
+
+
+# read csv and show the dataframe
+df = pd.read_csv(r'D:\train.csv')
+
+
+# create a figure with three subplots, size should be (15, 5)
+# show the box plot for ticket price with different classes
+# you need to set the x labels and y labels
+# a sample diagram is shown below
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
+
+sns.boxplot(x='Pclass', y='Fare', data=df[df['Pclass'] == 1], ax=axes[0])
+sns.boxplot(x='Pclass', y='Fare', data=df[df['Pclass'] == 2], ax=axes[1])
+sns.boxplot(x='Pclass', y='Fare', data=df[df['Pclass'] == 3], ax=axes[2])
+
+axes[0].set_title('Ticket Price for Pclass 1')
+axes[0].set_xlabel('PClass')
+axes[0].set_ylabel('Fare')
+
+axes[1].set_title('Ticket Price for Pclass 2')
+axes[1].set_xlabel('PClass')
+axes[1].set_ylabel('Fare')
+
+axes[2].set_title('Ticket Price for Pclass 3')
+axes[2].set_xlabel('PClass')
+axes[2].set_ylabel('Fare')
+
+plt.tight_layout()
+plt.show()
+
