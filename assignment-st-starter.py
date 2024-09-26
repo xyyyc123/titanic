aac0d7eb142f7a55958ebcbf8206298e8d1ddf84
xyyyc123 <1443268879@qq.com>
2024-09-26 23:38:51 +0800

    app_titanic.py

--- assignment-st-starter.py
+++ assignment-st-starter.py
@@ -19,21 +19,21 @@ st.dataframe(df)
 # a sample diagram is shown below
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
 
-sns.boxplot(x='Pclass', y='Fare', data=df[df['Pclass'] == 1], ax=axes[0])
-sns.boxplot(x='Pclass', y='Fare', data=df[df['Pclass'] == 2], ax=axes[1])
-sns.boxplot(x='Pclass', y='Fare', data=df[df['Pclass'] == 3], ax=axes[2])
+sns.boxplot(x='Pclass', y='Fare', data=df_train[df_train['Pclass'] == 1], ax=axes[0])
+sns.boxplot(x='Pclass', y='Fare', data=df_train[df_train['Pclass'] == 2], ax=axes[1])
+sns.boxplot(x='Pclass', y='Fare', data=df_train[df_train['Pclass'] == 3], ax=axes[2])
 
-axes[0].set_title('Ticket Price for Pclass 1')
-axes[0].set_xlabel('PClass')
-axes[0].set_ylabel('Fare')
+axes[0].set_title('Ticket Price for Pclass 1',fontsize=16)
+axes[0].set_xlabel('PClass',fontsize=12)
+axes[0].set_ylabel('Fare',fontsize=12)
 
-axes[1].set_title('Ticket Price for Pclass 2')
-axes[1].set_xlabel('PClass')
-axes[1].set_ylabel('Fare')
+axes[1].set_title('Ticket Price for Pclass 2',fontsize=16)
+axes[1].set_xlabel('PClass',fontsize=12)
+axes[1].set_ylabel('Fare',fontsize=12)
 
-axes[2].set_title('Ticket Price for Pclass 3')
-axes[2].set_xlabel('PClass')
-axes[2].set_ylabel('Fare')
+axes[2].set_title('Ticket Price for Pclass 3',fontsize=16)
+axes[2].set_xlabel('PClass',fontsize=12)
+axes[2].set_ylabel('Fare',fontsize=12)
 
 plt.tight_layout()
 plt.show()
